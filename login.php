0b6831a4a53e0b0ca8853f4eeaccc5c1f0ac6be3
augustoandrade13 <80776164+augustoandrade13@users.noreply.github.com>
2022-11-16 22:37:39 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+
+$login = $_POST['login'];
+$senha = MD5($_POST['senha']);
+$entrar = $_POST['entrar'];
+$connect = new mysqli('localhost','root','','loginsystem ');
+
+if(isset($entrar)){
+    $verifica = $connect -> query("SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'")
+    or die ("erro ao selecionar coluna");
+
+    $rows = $verifica -> num_rows;
+    if($rows <=0){
+        echo"<script language='javascript' type='text/javascript'>
+    }
+    alert('login e / ou senha incorretos');windows.location.href='login.html';</script>";
+    die();
+    
+    }else{
+        setcookie("login",$login);
+        header("location:index.php");
+    }
+
+}
+
+
+?>
